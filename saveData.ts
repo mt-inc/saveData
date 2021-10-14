24228f7b98dd4ca15a8b22f5086b197a0245fc6d
Maksym Shamko <max.shamko25@gmail.com>
2021-10-14 19:25:45 +0300

    working

--- saveData.ts
+++ saveData.ts
@@ -14,17 +14,22 @@ type Pairs = constants.Pairs;
 
 class saveData {
   private client;
-  private count: { [x in Pairs]: number };
   private usablePair: Pairs[];
-  private basefilenames: { [x in Pairs]: string };
-  private filenames: { [x in Pairs]: string };
   private filelength: number;
-  private totalCount: { [x in Pairs]: number };
   private io: Server;
   private timer: NodeJS.Timer | null;
-  private streams: { [x in Pairs]: fs.WriteStream | null };
-  private lastData: { [x in Pairs]: number };
   private watchdog: NodeJS.Timer | null;
+  private mainData: {
+    basefilenames: { [x in Pairs]: string };
+    filenames: { [x in Pairs]: string };
+    count: { [x in Pairs]: number };
+    totalCount: { [x in Pairs]: number };
+    lastData: { [x in Pairs]: number };
+    writeStreams: { [x in Pairs]: fs.WriteStream | null };
+    isRestart: { [x in Pairs]: boolean };
+    restartTime: { [x in Pairs]: number };
+    ws: { [x in Pairs]: (() => void) | null };
+  };
   constructor() {
     this.client = Binance();
     this.usablePair = [
@@ -41,100 +46,52 @@ class saveData {
       'SOLUSDT',
       'XRPUSDT',
     ];
-    this.basefilenames = {
-      BTCUSDT: '../trades/btc/btcusdt-',
-      BNBUSDT: '../trades/bnb/bnbusdt-',
-      ETHUSDT: '../trades/eth/ethusdt-',
-      ADAUSDT: '../trades/ada/adausdt-',
-      DOGEUSDT: '../trades/doge/dogeusdt-',
-      DOTUSDT: '../trades/dot/dotusdt-',
-      BTCBUSD: '../trades/btcb/btcbusd-',
-      ETHBUSD: '../trades/ehtb/ethbusd-',
-      BNBBUSD: '../trades/bnbb/bnbbusd-',
-      DOGEBUSD: '../trades/dogeb/dogebusd-',
-      SOLUSDT: '../trades/sol/solusdt-',
-      XRPUSDT: '../trades/xrp/xrpusdt-',
+
+    this.mainData = {
+      //@ts-ignore
+      basefilenames: {},
+      //@ts-ignore
+      filenames: {},
+      //@ts-ignore
+      count: {},
+      //@ts-ignore
+      totalCount: {},
+      //@ts-ignore,
+      writeStreams: {},
+      //@ts-ignore
+      lastData: {},
+      //@ts-ignore
+      isRestart: {},
+      //@ts-ignore
+      ws: {},
+      //@ts-ignore
+      restartTime: {},
     };
-    this.filenames = {
-      BTCUSDT: `${new Date().getTime()}.csv`,
-      BNBUSDT: `${new Date().getTime()}.csv`,
-      ETHUSDT: `${new Date().getTime()}.csv`,
-      ADAUSDT: `${new Date().getTime()}.csv`,
-      DOGEUSDT: `${new Date().getTime()}.csv`,
-      DOTUSDT: `${new Date().getTime()}.csv`,
-      BTCBUSD: `${new Date().getTime()}.csv`,
-      ETHBUSD: `${new Date().getTime()}.csv`,
-      BNBBUSD: `${new Date().getTime()}.csv`,
-      DOGEBUSD: `${new Date().getTime()}.csv`,
-      SOLUSDT: `${new Date().getTime()}.csv`,
-      XRPUSDT: `${new Date().getTime()}.csv`,
-    };
+    this.usablePair.map((pair) => {
+      let trimmed = pair.replace('USDT', '').replace('BUSD', 'b').toLowerCase();
+      if (trimmed === 'ethb') {
+        trimmed = 'ehtb';
+      }
+      this.mainData.basefilenames[pair] = `../trades/${trimmed}/${pair.toLowerCase()}-`;
+      this.mainData.filenames[pair] = `${new Date().getTime()}.csv`;
+      this.mainData.count[pair] = 0;
+      this.mainData.totalCount[pair] = 0;
+      this.mainData.writeStreams[pair] = null;
+      this.mainData.lastData[pair] = 0;
+      this.mainData.isRestart[pair] = false;
+      this.mainData.ws[pair] = null;
+      this.mainData.restartTime[pair] = 0;
+    });
     this.filelength = 200000;
-    this.count = {
-      BTCUSDT: 0,
-      BNBUSDT: 0,
-      ETHUSDT: 0,
-      ADAUSDT: 0,
-      DOGEUSDT: 0,
-      DOTUSDT: 0,
-      BTCBUSD: 0,
-      ETHBUSD: 0,
-      BNBBUSD: 0,
-      DOGEBUSD: 0,
-      SOLUSDT: 0,
-      XRPUSDT: 0,
-    };
-    this.totalCount = {
-      BTCUSDT: 0,
-      BNBUSDT: 0,
-      ETHUSDT: 0,
-      ADAUSDT: 0,
-      DOGEUSDT: 0,
-      DOTUSDT: 0,
-      BTCBUSD: 0,
-      BNBBUSD: 0,
-      ETHBUSD: 0,
-      DOGEBUSD: 0,
-      SOLUSDT: 0,
-      XRPUSDT: 0,
-    };
-    this.lastData = {
-      BTCUSDT: 0,
-      BNBUSDT: 0,
-      ETHUSDT: 0,
-      ADAUSDT: 0,
-      DOGEUSDT: 0,
-      DOTUSDT: 0,
-      BTCBUSD: 0,
-      BNBBUSD: 0,
-      ETHBUSD: 0,
-      DOGEBUSD: 0,
-      SOLUSDT: 0,
-      XRPUSDT: 0,
-    };
     this.io = new Server();
     this.io.listen(settings.ioport);
     this.timer = null;
-    this.streams = {
-      BTCUSDT: null,
-      BNBUSDT: null,
-      ETHUSDT: null,
-      ADAUSDT: null,
-      DOGEUSDT: null,
-      DOTUSDT: null,
-      BTCBUSD: null,
-      ETHBUSD: null,
-      BNBBUSD: null,
-      DOGEBUSD: null,
-      SOLUSDT: null,
-      XRPUSDT: null,
-    };
     this.watchdog = null;
   }
   start() {
     console.log(`${new Time().format(new Date().getTime())}: starting`);
-    Object.keys(this.basefilenames)
-      .map((item) => this.basefilenames[item])
+    Object.keys(this.mainData.basefilenames)
+      .map((item) => this.mainData.basefilenames[item])
       .map((item) => item.split('/'))
       .map((item) => {
         item.map((dir, ind) => {
@@ -155,57 +112,69 @@ class saveData {
     console.log(`${new Time().format(new Date().getTime())}: directory prepared`);
     const cbWs = (trade: AggregatedTrade) => {
       const pair = trade.symbol as Pairs;
-      const basefile = this.basefilenames[pair];
-      let suffix = this.filenames[pair];
-      if (this.count[pair] > this.filelength) {
-        this.streams[pair]?.close();
-        this.count[pair] = 0;
-        this.streams[pair] = null;
-        this.filenames[pair] = `${new Date().getTime()}.csv`;
-        suffix = this.filenames[pair];
+      const basefile = this.mainData.basefilenames[pair];
+      let suffix = this.mainData.filenames[pair];
+      if (this.mainData.count[pair] > this.filelength) {
+        this.mainData.writeStreams[pair]?.close();
+        this.mainData.count[pair] = 0;
+        this.mainData.writeStreams[pair] = null;
+        this.mainData.filenames[pair] = `${new Date().getTime()}.csv`;
+        suffix = this.mainData.filenames[pair];
       }
-      if (!this.streams[pair]) {
-        this.streams[pair] = fs.createWriteStream(`${basefile}${suffix}`);
-        this.streams[pair]?.on('error', (err) => {
+      if (!this.mainData.writeStreams[pair]) {
+        this.mainData.writeStreams[pair] = fs.createWriteStream(`${basefile}${suffix}`);
+        this.mainData.writeStreams[pair]?.on('error', (err) => {
           if (err) {
             console.log(err);
           }
         });
       }
-      this.lastData[pair] = trade.timestamp;
+      this.mainData.isRestart[pair] = false;
+      this.mainData.lastData[pair] = trade.timestamp;
       const data = [trade.price, trade.quantity, trade.timestamp];
-      this.streams[pair]?.write(`${data.join(',')}\n`);
-      this.count[pair]++;
-      this.totalCount[pair]++;
+      this.mainData.writeStreams[pair]?.write(`${data.join(',')}\n`);
+      this.mainData.count[pair]++;
+      this.mainData.totalCount[pair]++;
       this.io.emit(pair, trade);
     };
-    let t = this.client.ws.futuresAggTrades(this.usablePair, cbWs);
+    this.usablePair.map((pair) => {
+      this.mainData.ws[pair] = this.client.ws.futuresAggTrades(pair, cbWs);
+    });
+    const reconnect = (pair: Pairs, now: number, reason?: string) => {
+      if (typeof this.mainData.ws[pair] === 'function') {
+        //@ts-ignore
+        this.mainData.ws[pair]();
+      }
+      console.log(
+        `${new Time().format(new Date().getTime())}: ${pair} - ${
+          reason ?? `${(now - this.mainData.lastData[pair]).toLocaleString()} ms`
+        }`,
+      );
+      this.mainData.count[pair] = 200001;
+      this.mainData.lastData[pair] = 0;
+      this.mainData.ws[pair] = this.client.ws.futuresAggTrades(pair, cbWs);
+      this.mainData.isRestart[pair] = true;
+      this.mainData.restartTime[pair] = new Date().getTime();
+    };
     this.watchdog = setInterval(() => {
       const now = new Date().getTime();
-      let reason = '';
-      let rec = false;
-      Object.keys(this.lastData).map((pair) => {
-        const time = pair.indexOf('BUSD') !== -1 ? 30 * 1000 : 10 * 1000;
-        if (this.lastData[pair] > 0 && now - this.lastData[pair] > time) {
-          reason = `${reason},${pair} (${now - this.lastData[pair]}ms)`;
-          rec = true;
+      Object.keys(this.mainData.lastData).map((pair) => {
+        if (this.mainData.isRestart[pair] && now - this.mainData.restartTime[pair] > 5 * 1000) {
+          return reconnect(
+            pair,
+            now,
+            `reconnection timeout ${(now - this.mainData.restartTime[pair]).toLocaleString()} ms`,
+          );
+        }
+        const time = pair.indexOf('BUSD') !== -1 ? (pair === 'DOGEBUSD' ? 40 * 1000 : 30 * 1000) : 15 * 1000;
+        if (this.mainData.lastData[pair] > 0 && now - this.mainData.lastData[pair] > time) {
+          return reconnect(pair, now);
         }
       });
-      if (rec) {
-        console.log(`${new Date()} reconnecting ${reason}`);
-        t();
-        Object.keys(this.lastData).map((pair) => {
-          this.lastData[pair] = 0;
-        });
-        Object.keys(this.count).map((pair) => {
-          this.count[pair] = 200001;
-        });
-        t = this.client.ws.futuresAggTrades(this.usablePair, cbWs);
-      }
     }, 10 * 1000);
     this.timer = setInterval(() => {
-      const d = Object.keys(this.totalCount)
-        .map((item) => `${item}: ${this.totalCount[item]}`)
+      const d = Object.keys(this.mainData.totalCount)
+        .map((item) => `${item}: ${this.mainData.totalCount[item]}`)
         .join(', ');
       console.log(`${new Time().format(new Date().getTime())}: ${d}`);
     }, 4 * 60 * 60 * 1000);
@@ -219,9 +188,15 @@ class saveData {
       clearInterval(this.watchdog);
       this.watchdog = null;
     }
-    Object.keys(this.streams).map((pair) => {
-      if (this.streams[pair]) {
-        this.streams[pair]?.close();
+    Object.keys(this.mainData.writeStreams).map((pair) => {
+      if (this.mainData.writeStreams[pair]) {
+        this.mainData.writeStreams[pair]?.close();
+      }
+    });
+    Object.keys(this.mainData.ws).map((pair) => {
+      if (typeof this.mainData.ws[pair] === 'function') {
+        //@ts-ignore
+        this.mainData.ws[pair]();
       }
     });
   }
